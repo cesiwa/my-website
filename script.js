910149c2efa5e287afa5a1135edd5f142eb4de9c
cesiwa <mahimsila@gmail.com>
2022-06-25 12:08:15 +0300

    commit

---
+++ script.js
@@ -0,0 +1,71 @@
+$(".cesiwa").mouseenter(function(){
+    $(this).css("font-size","120px")
+});
+$(".cesiwa").mouseleave(function(){
+    $(this).css("font-size","60px")
+});
+
+
+$(".html").mouseenter(function(){
+    $(this).css("background-color","white")
+});
+$(".html").mouseleave(function(){
+    $(this).css("background-color","#222222")
+});
+
+$(".css").mouseenter(function(){
+    $(this).css("background-color","white")
+});
+$(".css").mouseleave(function(){
+    $(this).css("background-color","#222222")
+});
+
+$(".javascript").mouseenter(function(){
+    $(this).css("background-color","white")
+});
+$(".javascript").mouseleave(function(){
+    $(this).css("background-color","#222222")
+});
+
+$(".jquery").mouseenter(function(){
+    $(this).css("background-color","white")
+});
+$(".jquery").mouseleave(function(){
+    $(this).css("background-color","#222222")
+});
+$(".python").mouseenter(function(){
+    $(this).css("background-color","white")
+});
+$(".python").mouseleave(function(){
+    $(this).css("background-color","#222222")
+});
+
+
+$(".skillbox1").mouseenter(function(){
+    $(this).css("background-color","#222222");
+    $(".todolisttext").css("color","white")
+})
+$(".skillbox1").mouseleave(function(){
+    $(this).css("background-color","white");
+    $(".todolisttext").css("color","black")
+});
+
+
+$(".skillbox2").mouseenter(function(){
+    $(this).css("background-color","#222222");
+    $(".rockpapertext").css("color","white")
+})
+$(".skillbox2").mouseleave(function(){
+    $(this).css("background-color","white");
+    $(".rockpapertext").css("color","black")
+});
+
+
+$(".skillbox3").mouseenter(function(){
+    $(this).css("background-color","#222222");
+    $(".quotetext").css("color","white")
+})
+$(".skillbox3").mouseleave(function(){
+    $(this).css("background-color","white");
+    $(".quotetext").css("color","black")
+})
